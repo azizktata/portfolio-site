0166a64d48070bc8f8f4395b69cb2b152837abc4
azizktata <65952489+azizktata@users.noreply.github.com>
2024-09-25 15:21:25 +0100

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import {defineConfig} from "vite"
-
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 export default defineConfig({
-	plugins: [
-		
-	]
-})
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                project1: resolve(__dirname, 'project-1.html'),
+                project2: resolve(__dirname, 'project-2.html'),
+                project3: resolve(__dirname, 'project-3.html'),
+                project4: resolve(__dirname, 'project-4.html'),
+
+            }
+        }
+    },
+});
